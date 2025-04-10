a69c5414cbebedb48b30b109965b6932d3882e29
Panagiotis Doupidis <72436005+pdoup@users.noreply.github.com>
2025-04-10 19:38:18 +0300

    add gui consts

--- constants.py
+++ constants.py
@@ -25,3 +25,22 @@ STATE_SIZE = 3 + 4 + 4 + 2
 # Logging configuration
 LOG_FILE = f"snake_dqn_training_{time_ns()}.log"
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(name)s - [%(module)s:%(funcName)s:%(lineno)d] - %(message)s - [Process:%(process)d Thread:%(threadName)s]"
+
+# GUI configuration
+COLOR_BACKGROUND = (40, 40, 40)
+COLOR_SNAKE_BODY = (0, 200, 0)
+COLOR_SNAKE_HEAD = (0, 255, 0)
+COLOR_FOOD = (220, 0, 0)
+COLOR_TEXT = (230, 230, 230)
+COLOR_GAMEOVER = (255, 60, 60)
+
+CELL_SIZE = 20
+SCORE_AREA_HEIGHT = 30
+
+GRID_WIDTH_H = GRID_WIDTH
+GRID_HEIGHT_H = GRID_HEIGHT + GRID_HEIGHT * .9
+WINDOW_WIDTH = GRID_WIDTH_H * CELL_SIZE
+WINDOW_HEIGHT = GRID_HEIGHT_H * CELL_SIZE + SCORE_AREA_HEIGHT
+
+CELL_RADIUS = CELL_SIZE // 2
+OFFSET = CELL_SIZE // 2
